1f192f73b8dca6c4292258c519033f9a4be76f7f
Mateus <mateus.manoel1998@gmail.com>
2023-10-02 20:34:54 -0400

    começo da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroProdutoComponent } from './components/cadastro-produto/cadastro-produto.component';
+import { ListagemProdutoComponent } from './components/listagem-produto/listagem-produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroProdutoComponent,
+    ListagemProdutoComponent
   ],
   imports: [
     BrowserModule,
